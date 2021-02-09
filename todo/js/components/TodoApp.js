5b36a203f78f368773a77f2868f39946b2157db2
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-02-08 20:17:17 -0500

    trying module within app folder

--- todo/js/components/TodoApp.js
+++ todo/js/components/TodoApp.js
@@ -17,7 +17,7 @@ import TodoTextInput from './TodoTextInput';
 
 import React from 'react';
 import {createFragmentContainer, commitMutation, graphql} from 'react-relay';
-import {PeriqlesForm} from '../../../module/index.js';
+import {PeriqlesForm} from '../../module/index.js';
 // import PeriqlesForm from './PeriqlesForm.js';
 // import periqlesFormWrapper from './PeriqlesForm.js';
 // import schema from './schema.js';
