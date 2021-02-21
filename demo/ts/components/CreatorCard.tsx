b26771e7ce8b6284200182c41162bd09d0f77908
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-02-21 00:35:10 -0500

    todo: broken img links

---
+++ demo/ts/components/CreatorCard.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable flowtype/object-type-delimiter */
+import * as React from 'react';
+
+interface AuthorCardProps {
+  key: number;
+  author: {
+    img: string;
+    name: string;
+    link: string;
+  }
+}
+
+const CreatorCard = ({author}: AuthorCardProps): JSX.Element => {
+  const {img, name, link} = author;
+
+  return (
+    <p className="AuthorCard">
+      <img className="author-img" 
+      src={img} 
+      alt={name}
+      width="75"
+      height="75"></img>
+      <a href={link} className="author-link">{name}</a>
+    </p>
+  );
+};
+
+export default CreatorCard;
