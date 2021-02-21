08c3c5e2b71e5b3cd389736f64ba8180bddcbe18
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-02-21 00:38:22 -0500

    todo: add link to npm package

--- demo/ts/components/LinksSection.tsx
+++ demo/ts/components/LinksSection.tsx
@@ -4,18 +4,25 @@ const LinksSection = (): JSX.Element => {
 
   return (
     <div className="LinksSection">
-      <a href="">
-        <img className="repo-logo marketing-logo" src="../../public/assets/npm-logo-black.svg" alt="NPM" />
-      </a>
-      <a href="https:\/\/github.com/oslabs-beta/periqles">
-        <img className="repo-logo marketing-logo" src="../../public/assets/Github-Mark-64px.png" alt="Github" />
-      </a>
-    
-      {/* <div className="PitchArea"> */}
       <h3 className="pitch" id="long-pitch">
         Periqles is great! Periqles is awesome! Give periqles a try today!
       </h3>
-      {/* </div> */}
+      <div className="repo-logos">
+        <a href="">
+          <img className="marketing-logo" 
+          src="../../public/assets/npm-logo-black.png" 
+          alt="NPM" 
+          width="75"
+          height="50"/>
+        </a>
+        <a href="https:\/\/github.com/oslabs-beta/periqles">
+          <img className="marketing-logo" 
+          src="../../public/assets/Github-Mark-64px.png" 
+          alt="Github" 
+          width="75"
+          height="75"/>
+        </a>
+      </div>
     </div>
   );
 };
