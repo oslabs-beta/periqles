9ec9af37b45e720f3c3e123aee4b21009598dff6
eeeeean <ian.michael.garrett@gmail.com>
2021-02-22 10:59:27 -0500

    built something

--- todo/dist/mutations/AddUserMutation.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace _default {
-    export { commit };
-}
-export default _default;
-declare function commit(environment: any, username: any, password: any, email: any, gender: any, pizzaTopping: any, age: any): any;
