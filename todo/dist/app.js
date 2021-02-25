55307c5c0627f8d3a794d6f5d4b691941e7bb177
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-02-25 10:56:21 -0500

    checked old todo folder out of git

--- todo/dist/app.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import UserProfile from './components/UserProfile';
-var rootElement = document.getElementById('root');
-if (rootElement) {
-    ReactDOM.render(React.createElement(React.StrictMode, null,
-        React.createElement(UserProfile, null)), rootElement);
-}
